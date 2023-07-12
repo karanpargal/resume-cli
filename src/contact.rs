ca787e830ed6d5134f924b67b33cb313ac5f3d9d
Karan <karanpargal007@gmail.com>
2023-07-13 00:31:21 +0530

    Add socials

--- src/contact.rs
+++ src/contact.rs
@@ -5,4 +5,6 @@ pub fn show_contact() {
     println!("You can contact me at:");
     println!("Email: {}" , "karanpargal007@gmail.com".bright_green());
     println!("Mobile: {}" , "+91 7006484652".bright_green());
+    println!("LinkedIn: {}" , "https://www.linkedin.com/in/karan-pargal-509813211/".bright_green());
+    println!("");
 }
--- src/main.rs
+++ src/main.rs
@@ -14,45 +14,51 @@ fn main() {
     println!("");
     println!("Hey there! I'm {}, a full stack web3 developer and currently learning new technologies.","Karan Pargal".bold().bright_yellow());
 
-    let options = vec!["About","Experience","Skills","Contact"];
+    let options = vec!["About","Experience","Skills","Contact","Exit"];
 
-    let choice = Select::new("What would you like to know?", options.clone()).prompt();
+    loop{
+        let choice = Select::new("What would you like to know?", options.clone()).prompt();
 
-    match choice {
-        Ok(choice) => {
-            if choice == options[0] {
-                println!("");
-                println!("I am a proficient and driven {} with extensive experience in emerging technologies.","full-stack developer".bold().bright_yellow());
-                println!("I possess diverse technical skills, including proficiency in programming languages such as {}.","C++, Python, JavaScript, Solidity, Rust, and more".bold().bright_yellow());
-                println!("With practical knowledge in {} I have the capacity to tackle complex projects in the tech industry.","blockchain, web3, and machine learning.".bold().bright_yellow());
-                println!("");
-                println!("I have honed my technical skills through {} at multiple companies, and {} and am excited to continue building my skills and contributing to {} in the tech industry.","internships".bold().bright_yellow(),"online courses".bold().bright_yellow(),"innovative projects".bold().bright_yellow());
-                println!("Along with my technical expertise, I possess strong soft skills such as {}. I am a team player and enjoy collaborating with others to achieve common goals.","communication, critical thinking, and problem-solving".bold().bright_yellow());
-                println!("Furthermore, I have demonstrated natural leadership skills through various group projects.");
-                println!("");
-            }
-            else if choice == options[1] {
-                let file_path = "./data/experience/experience.json".to_owned();
-                let contents = fs::read_to_string(file_path).expect("Couldn't find or load that file.");
-                let res = show_experience(&contents);
-                match res {
-                    Ok(_res) => println!(""),
-                    Err(_) => println!("Error in experience.rs"),
+        match choice {
+            Ok(choice) => {
+                if choice == options[0] {
+                    println!("");
+                    println!("I am a proficient and driven {} with extensive experience in emerging technologies.","full-stack developer".bold().bright_yellow());
+                    println!("I possess diverse technical skills, including proficiency in programming languages such as {}.","C++, Python, JavaScript, Solidity, Rust, and more".bold().bright_yellow());
+                    println!("With practical knowledge in {} I have the capacity to tackle complex projects in the tech industry.","blockchain, web3, and machine learning.".bold().bright_yellow());
+                    println!("");
+                    println!("I have honed my technical skills through {} at multiple companies, and {} and am excited to continue building my skills and contributing to {} in the tech industry.","internships".bold().bright_yellow(),"online courses".bold().bright_yellow(),"innovative projects".bold().bright_yellow());
+                    println!("Along with my technical expertise, I possess strong soft skills such as {}. I am a team player and enjoy collaborating with others to achieve common goals.","communication, critical thinking, and problem-solving".bold().bright_yellow());
+                    println!("Furthermore, I have demonstrated natural leadership skills through various group projects.");
+                    println!("");
                 }
-            }
-            else if choice == options[2] {
-                let file_path = "./data/skills/skills.json".to_owned();
-                let contents = fs::read_to_string(file_path).expect("Couldn't find or load that file.");
-                let res = show_skills(&contents);
-                match res {
-                    Ok(_res) => println!(""),
-                    Err(_) => println!("Error in experience.rs"),
+                else if choice == options[1] {
+                    let file_path = "./data/experience/experience.json".to_owned();
+                    let contents = fs::read_to_string(file_path).expect("Couldn't find or load that file.");
+                    let res = show_experience(&contents);
+                    match res {
+                        Ok(_res) => println!(""),
+                        Err(_) => println!("Error in experience.rs"),
+                    }
                 }
-            }
-            else if choice == options[3] {
-                show_contact();
-            }
-        },
-        Err(_) => println!("You did not select a valid option"),
+                else if choice == options[2] {
+                    let file_path = "./data/skills/skills.json".to_owned();
+                    let contents = fs::read_to_string(file_path).expect("Couldn't find or load that file.");
+                    let res = show_skills(&contents);
+                    match res {
+                        Ok(_res) => println!(""),
+                        Err(_) => println!("Error in experience.rs"),
+                    }
+                }
+                else if choice == options[3] {
+                    show_contact();
+                }
+                else if choice == options[4] {
+                    println!("Bye! Have a great day!");
+                    break;
+                }
+            },
+            Err(_) => println!("You did not select a valid option"),
+        }
     }
 }
